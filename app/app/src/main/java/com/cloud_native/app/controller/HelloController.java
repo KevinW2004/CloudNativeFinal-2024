c7772652b29361347113c4ea8e031ebae8b055cb
KevinW2004 <221250068@smail.nju.edu.cn>
2024-08-08 23:35:11 +0800

    feat: 也许实现了限流

--- app/app/src/main/java/com/cloud_native/app/controller/HelloController.java
+++ app/app/src/main/java/com/cloud_native/app/controller/HelloController.java
@@ -1,18 +1,24 @@
 /**
  * @auther KevinWu
- * @date 2024年08月08日 17:37
+ * @date 2024年08月08日 23:24
  */
 package com.cloud_native.app.controller;
 
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Collections;
+import java.util.Map;
+
 @RestController
-@RequestMapping("/hello")
+@RequestMapping("/api")
 public class HelloController {
-    @GetMapping("/")
-    public String hello() {
-        return "{\"msg\":\"hello\"}";
+
+    @RateLimiter(name = "helloLimiter")
+    @GetMapping("/hello")
+    public Map<String, String> sayHello() {
+        return Collections.singletonMap("msg", "hello");
     }
 }
